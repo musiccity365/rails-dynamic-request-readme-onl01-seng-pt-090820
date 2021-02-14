0f4d81d2933cfccf22431dabcd6ac2f8a7929115
musiccity365 <m_sackstein@yahoo.com>
2021-02-13 19:47:12 -0500

    commit tree 4ce7dc0ae24f99b6828e4019cb91251562deb3fb

--- spec/controllers/posts_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe PostsController, type: :controller do
-  it 'shows the description on the show page in a p tag' do
-    visit "/posts/#{@post.id}"
-    expect(page).to have_css("p", text: "My post desc")
-  end
-end
