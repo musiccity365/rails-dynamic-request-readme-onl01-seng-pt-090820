a0692db565784adbb801ebdc5f1bf66d99f2d997
musiccity365 <m_sackstein@yahoo.com>
2021-02-13 19:36:24 -0500

    Done.

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-
+  it 'shows the description on the show page in a p tag' do
+    visit "/posts/#{@post.id}"
+    expect(page).to have_css("p", text: "My post desc")
+  end
 end
